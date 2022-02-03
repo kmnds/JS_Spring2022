64f83e666684f3a9e717ea312fa630fa00f53975
marko <hyrrimarko@gmail.com>
2022-02-03 18:16:16 +0200

    First changes

---
+++ myjavascript.js
@@ -0,0 +1,25 @@
+function myFunction(){
+    alert("You clicked me! " + new Date());
+}
+
+function toggleTable() {
+    document.getElementById("example").classList.toggle("display");
+  }
+
+  function geolocation(){
+    alert(getCurrentPosition());
+}
+
+function whenhover(){
+ alert("Stepped over my a mouse!");
+
+};
+
+function whenleft(){
+  alert("Bye bye mouse!");
+ 
+ };
+
+ function whenfocus(){
+  document.write("Please fill in th form with proper data.");
+ };
